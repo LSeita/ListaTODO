2a0363a0c74d02d4e305c0d71142f1bf0b9b8d9d
LSeita <seita.lucca@gmail.com>
2023-07-17 17:17:34 -0300

    Adiciona html e css base

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    watchOptions: {
+        ignored: /node_modules/,
+        poll: true
+    },
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],  
+    },
+}
